b6b1bb2976aa11613f624e291d3c23da119bfc2e
artslimedev <102567983+artslimedev@users.noreply.github.com>
2022-09-26 00:49:40 -0400

    finished all the basic functionality

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const express = require("express")
 const path = require('path');
 const app = express()
 const cors = require('cors')
-const PORT = 6900;
+const PORT = process.env.PORT || 6900;
 const mongoose = require("mongoose")
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
+const methodOverride = require("method-override");
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -33,6 +34,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
 // Sessions
 app.use(
     session({
